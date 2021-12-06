e2ebc44a5b808546e903f6f29f16d67ab1672e39
Senad Zenkovic <zenkovic88@gmail.com>
2021-12-06 14:58:34 -0700

    complete employee

---
+++ employee.sql
@@ -0,0 +1,6 @@
+SELECT (first_name, last_name) FROM employee WHERE city='Calgary';
+SELECT * FROM employee ORDER BY birth_date DESC LIMIT 1;
+SELECT * FROM employee ORDER BY birth_date ASC LIMIT 1;
+SELECT * FROM employee WHERE first_name='Nancy' AND last_name='Edwards';
+SELECT * FROM employee WHERE reports_to=2;
+SELECT COUNT(first_name) FROM employee WHERE city='Lethbridge';
